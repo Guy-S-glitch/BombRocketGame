4298c12dc638a92b8be04cdca3764e1c25d3cf1b
Guy-S-glitch <guysolomon365@gmail.com>
2024-02-26 22:44:25 +0200

    added new partial class for menu's values

--- SnakeLadder/menu.xaml.cs
+++ SnakeLadder/menu.xaml.cs
@@ -23,26 +23,6 @@ namespace SnakeLadder
 {
     public partial class menu : Window
     {
-        Decoration decoration = new Decoration();
-        PreGame pregameLogic = new PreGame();
-
-       
-
-        private static Random _random = new Random();
-        
-        private static short _turn = 1;   //keep tracks of who's turn is it 
-        private static short _randomRocketBomb;    //used to contain the random amount of spaces the rocket/bomb send you
-        private static short _currentRow, _futureRow, _currentColumn, _futureColumn, _currentPlace, _nextPlace;   //used to navigate the character on the grid
-        private static short[] _bombRocketPlaces = { (short)RocketBomb.rocket1, (short)RocketBomb.rocket2, (short)RocketBomb.rocket3 , (short)RocketBomb.rocket4, 
-                                                     (short)RocketBomb.rocket5, (short)RocketBomb.rocket6, (short)RocketBomb.bomb1,  (short)RocketBomb.bomb2, 
-                                                     (short)RocketBomb.bomb3,  (short)RocketBomb.bomb4, (short)RocketBomb.bomb5 
-        };
-        private static bool _bombFlag = false, _boostFlag = false;   //if player land on bomb raise flag
-        private static bool _ricochet100Flag = false, _winFlag = false;   //flag raised to determain if the player surpass 100 and call relative animation
-
-        private static string _rollText = "You can roll", _waitText = "Wait";
-        private static string _exitMessage = "Are you sure you want to exit", _exitCaption = "Conform Exit", _exitYes = "thanks for playing", _exitNo = "returning to the game";
-        private static string _menuMessage = "Are you sure you want to return to the menu", _menuCaption = "Confirm Menu", _menuYes = "going back to menu", _menuNo = "returning to the game";
 
         private void How_much_SelectionChanged(object sender, SelectionChangedEventArgs e) { pregameLogic.How_much_SelectionChanged(sender, e, ref How_much, ref keep_data, ref select_players, ref info); }
         private void info_Click(object sender, RoutedEventArgs e) { pregameLogic.info_Click(sender, e, ref info, ref keep_data, ref Dice, ref turn_text, ref Game_Grid, ref RollOrWait, _rollText); }
